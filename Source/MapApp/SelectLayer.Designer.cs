c5d67a4513507b4ef93eb1ad8c6aad204f31c448
devgis <devgis@163.com>
2019-12-29 17:46:18 -0800

    添加selectlayer代码
    添加selectlayer代码

--- Source/MapApp/SelectLayer.Designer.cs
+++ Source/MapApp/SelectLayer.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.btOK = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.btOK = new System.Windows.Forms.Button();
-            this.btCancel = new System.Windows.Forms.Button();
+            this.cbLayers = new System.Windows.Forms.ComboBox();
+            this.cbPropertis = new System.Windows.Forms.ComboBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -50,8 +50,8 @@
             this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.label2, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cbLayers, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cbPropertis, 2, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
@@ -78,6 +78,26 @@
             this.panel1.Size = new System.Drawing.Size(400, 30);
             this.panel1.TabIndex = 0;
             // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(321, 3);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 23);
+            this.btCancel.TabIndex = 1;
+            this.btCancel.Text = "取消";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // btOK
+            // 
+            this.btOK.Location = new System.Drawing.Point(243, 3);
+            this.btOK.Name = "btOK";
+            this.btOK.Size = new System.Drawing.Size(75, 23);
+            this.btOK.TabIndex = 0;
+            this.btOK.Text = "确定";
+            this.btOK.UseVisualStyleBackColor = true;
+            this.btOK.Click += new System.EventHandler(this.btOK_Click);
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -96,41 +116,24 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "属性";
             // 
-            // comboBox1
+            // cbLayers
             // 
-            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(160, 95);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(294, 25);
-            this.comboBox1.TabIndex = 3;
+            this.cbLayers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbLayers.FormattingEnabled = true;
+            this.cbLayers.Location = new System.Drawing.Point(160, 95);
+            this.cbLayers.Name = "cbLayers";
+            this.cbLayers.Size = new System.Drawing.Size(294, 25);
+            this.cbLayers.TabIndex = 3;
+            this.cbLayers.SelectedValueChanged += new System.EventHandler(this.cbLayers_SelectedValueChanged);
             // 
-            // comboBox2
+            // cbPropertis
             // 
-            this.comboBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(160, 125);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(294, 25);
-            this.comboBox2.TabIndex = 4;
-            // 
-            // btOK
-            // 
-            this.btOK.Location = new System.Drawing.Point(243, 3);
-            this.btOK.Name = "btOK";
-            this.btOK.Size = new System.Drawing.Size(75, 23);
-            this.btOK.TabIndex = 0;
-            this.btOK.Text = "确定";
-            this.btOK.UseVisualStyleBackColor = true;
-            // 
-            // btCancel
-            // 
-            this.btCancel.Location = new System.Drawing.Point(321, 3);
-            this.btCancel.Name = "btCancel";
-            this.btCancel.Size = new System.Drawing.Size(75, 23);
-            this.btCancel.TabIndex = 1;
-            this.btCancel.Text = "取消";
-            this.btCancel.UseVisualStyleBackColor = true;
+            this.cbPropertis.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbPropertis.FormattingEnabled = true;
+            this.cbPropertis.Location = new System.Drawing.Point(160, 125);
+            this.cbPropertis.Name = "cbPropertis";
+            this.cbPropertis.Size = new System.Drawing.Size(294, 25);
+            this.cbPropertis.TabIndex = 4;
             // 
             // SelectLayer
             // 
@@ -144,6 +147,7 @@
             this.Name = "SelectLayer";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "选择图层";
+            this.Load += new System.EventHandler(this.SelectLayer_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.panel1.ResumeLayout(false);
@@ -158,8 +162,8 @@
         private System.Windows.Forms.Button btOK;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cbLayers;
+        private System.Windows.Forms.ComboBox cbPropertis;
         private System.Windows.Forms.Button btCancel;
     }
 }
--- Source/MapApp/SelectLayer.cs
+++ Source/MapApp/SelectLayer.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using DEVGIS.Common;
 using DEVGIS.MapAPP.Entities;
 
 namespace DEVGIS.MapAPP
@@ -28,6 +29,61 @@ namespace DEVGIS.MapAPP
         {
             InitializeComponent();
             layerItems=LayerItems;
+        }
+
+        private void SelectLayer_Load(object sender, EventArgs e)
+        {
+            cbLayers.DataSource = layerItems;
+            cbLayers.DisplayMember = "DisplayName";
+            cbLayers.ValueMember = "LayerName";
+        }
+
+        private void cbLayers_SelectedValueChanged(object sender, EventArgs e)
+        {
+            cbPropertis.Items.Clear();
+            if (cbLayers.SelectedItem != null&&cbLayers.SelectedItem is LayerItem)
+            {
+                var list = (cbLayers.SelectedItem as LayerItem).Propertys;
+                if (list != null && list.Count > 0)
+                {
+                    foreach (var prop in list)
+                    {
+                        cbPropertis.Items.Add(prop);
+                    }
+                }
+            }
+        }
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void btOK_Click(object sender, EventArgs e)
+        {
+            if (cbLayers.SelectedValue != null)
+            {
+                LayerName = cbLayers.SelectedValue.ToString();
+            }
+            else
+            {
+                MessageHelper.ShowError("请选择图层！");
+                return;
+            }
+
+            if (cbPropertis.SelectedText != null)
+            {
+                PropertyName = cbPropertis.SelectedText;
+            }
+            else
+            {
+                MessageHelper.ShowError("请选择属性！");
+                return;
+            }
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
         }
     }
 }
